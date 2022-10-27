68291e7a6c88386396d154d8ddf037368f94ea9b
Jaiden (Zuri) Smith <jaiden_smith@m018687.aw.org>
2022-10-27 11:22:23 -0700

    message

---
+++ problemset2.py
@@ -0,0 +1,24 @@
+'''#class definition
+
+class numerial:
+    # define a constructor numerial
+    x=(1)
+    def __init__(self):
+        self.number = 3*(3-x)
+    def getnumber(self):
+     return self.number
+    #main method
+        def main():'''
+
+'''def double_char(str):
+    Liststr=[]
+    for i in str:
+        Liststr.append(i)
+        Liststr.append(i)
+    return ''.join(Liststr)
+print(double_char(input("Enter string: ")))
+print(double_char(input("Enter string: ")))
+print(double_char(input("Enter string: ")))'''
+
+print PizzaStop
+
